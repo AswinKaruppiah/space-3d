ac750668d6d1710968261bad668061d955c3dcac
Aswin <89991183+AswinKaruppiah@users.noreply.github.com>
2025-05-22 19:23:48 +0530

    base hero section

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,9 @@ export default function Home() {
   return (
     <div className="relative h-full w-full">
       <Hero />
+      <div className="h-[1000vh]">
+
+      </div>
     </div>
   );
 }
--- src/section/hero/Hero.jsx
+++ src/section/hero/Hero.jsx
@@ -1,15 +1,24 @@
 'use client';
 
 import Spline from '@splinetool/react-spline';
+import { Suspense } from 'react';
 
 export default function Hero() {
 
   return (
     <div className="sp-hero-section">
-      <h1>Space</h1>
-      <div className="w-screen h-screen">
-        <Spline
-          scene="https://prod.spline.design/8XljPx8CoGV2Vbpf/scene.splinecode" />
+      <div className='w-full max-w-screen-2xl backdrop-blur-sm bg-transparent px-5 py-2'>
+        <h2>ASTO_</h2>
+      </div>
+      <div className='pt-40 w-full p-5 h-full'>
+        <h1>Unlock the Infinte Space</h1>
+      </div>
+      <div className="absolute top-[90vh] inset-0 origin-center left-0 h-full w-screen">
+        <Suspense fallback={<div className="w-full h-full animate-pulse bg-gray-200">Loading 3D Model...</div>}>
+          <Spline
+            scene="https://prod.spline.design/8XljPx8CoGV2Vbpf/scene.splinecode"
+          />
+        </Suspense>
       </div>
     </div>
   );
